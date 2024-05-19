04404c41c0240582782b7c8c03f9bfc664ee167a
t.chambard <t.chambard@bee-buzziness.com>
2024-05-20 01:12:32 +0200

    fix lint

--- packages/programs/soltrain-voting-program/types/soltrain_voting_program.ts
+++ packages/programs/soltrain-voting-program/types/soltrain_voting_program.ts
@@ -5,28 +5,19 @@
  * IDL can be found at `target/idl/soltrain_voting_program.json`.
  */
 export type SoltrainVotingProgram = {
-  "address": "Gma1Uuq1wjsg3gsbuaWoHki2QPtWn1ikD46s2WUTyWtE",
-  "metadata": {
-    "name": "soltrainVotingProgram",
-    "version": "0.1.0",
-    "spec": "0.1.0",
-    "description": "Created with Anchor"
-  },
-  "instructions": [
+  address: "Gma1Uuq1wjsg3gsbuaWoHki2QPtWn1ikD46s2WUTyWtE";
+  metadata: {
+    name: "soltrainVotingProgram";
+    version: "0.1.0";
+    spec: "0.1.0";
+    description: "Created with Anchor";
+  };
+  instructions: [
     {
-      "name": "initialize",
-      "discriminator": [
-        175,
-        175,
-        109,
-        31,
-        13,
-        152,
-        155,
-        237
-      ],
-      "accounts": [],
-      "args": []
-    }
-  ]
+      name: "initialize";
+      discriminator: [175, 175, 109, 31, 13, 152, 155, 237];
+      accounts: [];
+      args: [];
+    },
+  ];
 };
