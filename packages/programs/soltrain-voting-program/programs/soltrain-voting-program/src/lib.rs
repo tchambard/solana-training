c52408c076b3cad7cbd8408dd48b1bf4148bc50e
t.chambard <t.chambard@bee-buzziness.com>
2024-06-06 00:15:17 +0200

    First voting program implementation

--- packages/programs/soltrain-voting-program/programs/soltrain-voting-program/src/lib.rs
+++ packages/programs/soltrain-voting-program/programs/soltrain-voting-program/src/lib.rs
@@ -3,13 +3,99 @@ use anchor_lang::prelude::*;
 declare_id!("4PGnHfbudx56T214YReyJ25n3UCfLvWG5icWocESzH6n");
 
 #[program]
-pub mod soltrain_voting_program {
+pub mod voting {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_voting_session(
+        ctx: Context<InitSession>,
+        name: String,
+        description: String,
+    ) -> Result<()> {
+        let global_account = &mut ctx.accounts.global_account;
+        let session_account = &mut ctx.accounts.session_account;
+        session_account.status = WorkflowStatus::None;
+
+        session_account.session_id = global_account.session_count;
+        session_account.name = name;
+        session_account.description = description;
+        session_account.proposal_count = 0;
+
+        global_account.session_count += 1;
+        Ok(())
+    }
+
+    pub fn init_program(ctx: Context<InitGlobal>) -> Result<()> {
+        let global_account = &mut ctx.accounts.global_account;
+        global_account.session_count = 0;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitSession<'info> {
+    #[account(
+        init,
+        payer = owner,
+        space = 8 + SessionAccount::INIT_SPACE,
+        seeds = [
+            b"session".as_ref(),
+            &global_account.session_count.to_le_bytes()
+        ],
+        bump
+    )]
+    pub session_account: Account<'info, SessionAccount>,
+
+    #[account(mut, seeds = [b"global".as_ref()], bump)]
+    pub global_account: Account<'info, GlobalAccount>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitGlobal<'info> {
+    #[account(
+        init,
+        payer = owner,
+        space = 8 + GlobalAccount::INIT_SPACE,
+        seeds = [b"global".as_ref()],
+        bump,
+    )]
+    pub global_account: Account<'info, GlobalAccount>,
+
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct GlobalAccount {
+    pub session_count: u64,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct SessionAccount {
+    pub session_id: u64,
+    #[max_len(20)]
+    pub name: String,
+    #[max_len(80)]
+    pub description: String,
+    pub status: WorkflowStatus,
+    pub proposal_count: u64,
+}
+
+#[derive(Clone, AnchorSerialize, AnchorDeserialize, InitSpace)]
+pub enum WorkflowStatus {
+    None,
+    RegisteringVoters,
+    ProposalsRegistrationStarted,
+    ProposalsRegistrationEnded,
+    VotingSessionStarted,
+    VotingSessionEnded,
+    VotesTallied,
+}
---
+++ packages/programs/soltrain-voting-program/tests/voting.ts
@@ -0,0 +1,74 @@
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+import { Keypair, PublicKey } from '@solana/web3.js';
+
+import { Voting } from '../types/voting';
+
+const skipPreflight = false;
+
+export const generateSeededKeypair = (str: string) => {
+    return Keypair.fromSeed(
+        anchor.utils.bytes.utf8.encode(anchor.utils.sha256.hash(str)).slice(0, 32),
+    );
+};
+
+describe('voting', () => {
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
+
+    const program = anchor.workspace.Voting as Program<Voting>;
+
+    // const sessionAccount = anchor.web3.Keypair.generate();
+    const owner = provider.wallet as anchor.Wallet;
+
+    describe('> Voting actions are conditionned by voting session status', () => {
+
+        const [globalPDA] = PublicKey.findProgramAddressSync([Buffer.from('global')], program.programId);
+
+        beforeEach(async () => {
+            await program.methods
+                .initProgram()
+                .accounts({
+                    owner: owner.publicKey,
+                    globalAccount: globalPDA,
+                })
+                // .signers([sessionAccount])
+                .rpc({ skipPreflight });
+
+            const globalAccount = await program.account.globalAccount.fetch(
+                globalPDA,
+            );
+
+            console.log(`sessionCount: ${JSON.stringify(globalAccount.sessionCount)}`);
+
+            const [sessionPDA] = PublicKey.findProgramAddressSync(
+                [Buffer.from('session'), globalAccount.sessionCount.toBuffer('le', 8)],
+                program.programId,
+            );
+
+            // Invoke the initialize instruction
+            await program.methods
+                .createVotingSession(
+                    'Super Heroes',
+                    'A vote for every superheroes to find who will rule the world',
+                )
+                .accounts({
+                    owner: owner.publicKey,
+                    sessionAccount: sessionPDA,
+                    globalAccount: globalPDA,
+                })
+                // .signers([sessionAccount])
+                .rpc({ skipPreflight });
+
+            const votingSession = await program.account.sessionAccount.fetch(
+                sessionPDA,
+            );
+
+            console.log(`Status: ${JSON.stringify(votingSession.status)}`);
+        });
+
+        it('test', () => {
+            console.log('ok');
+        });
+    });
+});
