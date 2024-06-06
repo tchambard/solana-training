4186b7e71537a8ea63f9b5c823b13214248684a9
t.chambard <t.chambard@bee-buzziness.com>
2024-06-06 02:26:47 +0200

    fix tests

--- packages/programs/soltrain-voting-program/types/voting.ts
+++ packages/programs/soltrain-voting-program/types/voting.ts
@@ -5,180 +5,238 @@
  * IDL can be found at `target/idl/voting.json`.
  */
 export type Voting = {
-	address: '4PGnHfbudx56T214YReyJ25n3UCfLvWG5icWocESzH6n';
-	metadata: {
-		name: 'voting';
-		version: '0.1.0';
-		spec: '0.1.0';
-		description: 'Created with Anchor';
-	};
-	instructions: [
-		{
-			name: 'createVotingSession';
-			discriminator: [241, 56, 27, 243, 109, 166, 75, 93];
-			accounts: [
-				{
-					name: 'sessionAccount';
-					writable: true;
-					pda: {
-						seeds: [
-							{
-								kind: 'const';
-								value: [115, 101, 115, 115, 105, 111, 110];
-							},
-							{
-								kind: 'account';
-								path: 'global_account.session_count';
-								account: 'globalAccount';
-							},
-						];
-					};
-				},
-				{
-					name: 'globalAccount';
-					writable: true;
-					pda: {
-						seeds: [
-							{
-								kind: 'const';
-								value: [103, 108, 111, 98, 97, 108];
-							},
-						];
-					};
-				},
-				{
-					name: 'owner';
-					writable: true;
-					signer: true;
-				},
-				{
-					name: 'systemProgram';
-					address: '11111111111111111111111111111111';
-				},
-			];
-			args: [
-				{
-					name: 'name';
-					type: 'string';
-				},
-				{
-					name: 'description';
-					type: 'string';
-				},
-			];
-		},
-		{
-			name: 'initProgram';
-			discriminator: [56, 120, 211, 99, 196, 190, 129, 187];
-			accounts: [
-				{
-					name: 'globalAccount';
-					writable: true;
-					pda: {
-						seeds: [
-							{
-								kind: 'const';
-								value: [103, 108, 111, 98, 97, 108];
-							},
-						];
-					};
-				},
-				{
-					name: 'owner';
-					writable: true;
-					signer: true;
-				},
-				{
-					name: 'systemProgram';
-					address: '11111111111111111111111111111111';
-				},
-			];
-			args: [];
-		},
-	];
-	accounts: [
-		{
-			name: 'globalAccount';
-			discriminator: [129, 105, 124, 171, 189, 42, 108, 69];
-		},
-		{
-			name: 'sessionAccount';
-			discriminator: [74, 34, 65, 133, 96, 163, 80, 69];
-		},
-	];
-	types: [
-		{
-			name: 'globalAccount';
-			type: {
-				kind: 'struct';
-				fields: [
-					{
-						name: 'sessionCount';
-						type: 'u64';
-					},
-				];
-			};
-		},
-		{
-			name: 'sessionAccount';
-			type: {
-				kind: 'struct';
-				fields: [
-					{
-						name: 'sessionId';
-						type: 'u64';
-					},
-					{
-						name: 'name';
-						type: 'string';
-					},
-					{
-						name: 'description';
-						type: 'string';
-					},
-					{
-						name: 'status';
-						type: {
-							defined: {
-								name: 'workflowStatus';
-							};
-						};
-					},
-					{
-						name: 'proposalCount';
-						type: 'u64';
-					},
-				];
-			};
-		},
-		{
-			name: 'workflowStatus';
-			type: {
-				kind: 'enum';
-				variants: [
-					{
-						name: 'none';
-					},
-					{
-						name: 'registeringVoters';
-					},
-					{
-						name: 'proposalsRegistrationStarted';
-					},
-					{
-						name: 'proposalsRegistrationEnded';
-					},
-					{
-						name: 'votingSessionStarted';
-					},
-					{
-						name: 'votingSessionEnded';
-					},
-					{
-						name: 'votesTallied';
-					},
-				];
-			};
-		},
-	];
+  "address": "4PGnHfbudx56T214YReyJ25n3UCfLvWG5icWocESzH6n",
+  "metadata": {
+    "name": "voting",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
+    {
+      "name": "createVotingSession",
+      "discriminator": [
+        241,
+        56,
+        27,
+        243,
+        109,
+        166,
+        75,
+        93
+      ],
+      "accounts": [
+        {
+          "name": "sessionAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  115,
+                  101,
+                  115,
+                  115,
+                  105,
+                  111,
+                  110
+                ]
+              },
+              {
+                "kind": "account",
+                "path": "global_account.session_count",
+                "account": "globalAccount"
+              }
+            ]
+          }
+        },
+        {
+          "name": "globalAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  103,
+                  108,
+                  111,
+                  98,
+                  97,
+                  108
+                ]
+              }
+            ]
+          }
+        },
+        {
+          "name": "owner",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": [
+        {
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "name": "description",
+          "type": "string"
+        }
+      ]
+    },
+    {
+      "name": "initProgram",
+      "discriminator": [
+        56,
+        120,
+        211,
+        99,
+        196,
+        190,
+        129,
+        187
+      ],
+      "accounts": [
+        {
+          "name": "globalAccount",
+          "writable": true,
+          "pda": {
+            "seeds": [
+              {
+                "kind": "const",
+                "value": [
+                  103,
+                  108,
+                  111,
+                  98,
+                  97,
+                  108
+                ]
+              }
+            ]
+          }
+        },
+        {
+          "name": "owner",
+          "writable": true,
+          "signer": true
+        },
+        {
+          "name": "systemProgram",
+          "address": "11111111111111111111111111111111"
+        }
+      ],
+      "args": []
+    }
+  ],
+  "accounts": [
+    {
+      "name": "globalAccount",
+      "discriminator": [
+        129,
+        105,
+        124,
+        171,
+        189,
+        42,
+        108,
+        69
+      ]
+    },
+    {
+      "name": "sessionAccount",
+      "discriminator": [
+        74,
+        34,
+        65,
+        133,
+        96,
+        163,
+        80,
+        69
+      ]
+    }
+  ],
+  "types": [
+    {
+      "name": "globalAccount",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "sessionCount",
+            "type": "u64"
+          }
+        ]
+      }
+    },
+    {
+      "name": "sessionAccount",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "sessionId",
+            "type": "u64"
+          },
+          {
+            "name": "name",
+            "type": "string"
+          },
+          {
+            "name": "description",
+            "type": "string"
+          },
+          {
+            "name": "status",
+            "type": {
+              "defined": {
+                "name": "workflowStatus"
+              }
+            }
+          },
+          {
+            "name": "proposalCount",
+            "type": "u64"
+          }
+        ]
+      }
+    },
+    {
+      "name": "workflowStatus",
+      "type": {
+        "kind": "enum",
+        "variants": [
+          {
+            "name": "none"
+          },
+          {
+            "name": "registeringVoters"
+          },
+          {
+            "name": "proposalsRegistrationStarted"
+          },
+          {
+            "name": "proposalsRegistrationEnded"
+          },
+          {
+            "name": "votingSessionStarted"
+          },
+          {
+            "name": "votingSessionEnded"
+          },
+          {
+            "name": "votesTallied"
+          }
+        ]
+      }
+    }
+  ]
 };
