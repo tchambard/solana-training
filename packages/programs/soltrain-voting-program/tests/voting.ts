8380c42eab5ef41604d7ef4159c63984b0582ffe
t.chambard <t.chambard@bee-buzziness.com>
2024-06-11 00:04:28 +0200

    feat(voting-program): add createVotingSession function to simplify tests

--- packages/programs/soltrain-voting-program/tests/voting.ts
+++ packages/programs/soltrain-voting-program/tests/voting.ts
@@ -1,9 +1,9 @@
 import * as anchor from '@coral-xyz/anchor';
 import { BorshCoder, EventParser, Program } from '@coral-xyz/anchor';
-import { PublicKey } from '@solana/web3.js';
+import { Keypair, PublicKey } from '@solana/web3.js';
 import { assert } from 'chai';
 
-import { Voting } from '../types/voting';
+import { Voting } from '../client/types/voting';
 
 const skipPreflight = false;
 
@@ -39,105 +39,124 @@ describe('voting', () => {
 	anchor.setProvider(provider);
 
 	const program = anchor.workspace.Voting as Program<Voting>;
+	const connection = program.provider.connection;
 
-	// const sessionAccount = anchor.web3.Keypair.generate();
-	const owner = provider.wallet as anchor.Wallet;
+	const administrator = provider.wallet as anchor.Wallet;
+	const batman = anchor.web3.Keypair.generate();
 
 	const [globalPDA] = PublicKey.findProgramAddressSync(
 		[Buffer.from('global')],
 		program.programId,
 	);
-	let globalAccount: IGlobalAccount;
+
+	const createVotingSession = async (
+		owner: Keypair,
+		name: string,
+		description: string,
+	) => {
+		const sessionId =
+			(await program.account.globalAccount.fetch(globalPDA)).sessionCount ||
+			new anchor.BN(0);
+
+		const [pda] = PublicKey.findProgramAddressSync(
+			[Buffer.from('session'), sessionId.toBuffer('le', 8)],
+			program.programId,
+		);
+
+		const tx = await program.methods
+			.createVotingSession(name, description)
+			.accounts({
+				owner: owner.publicKey,
+				sessionAccount: pda,
+				globalAccount: globalPDA,
+			})
+			.signers([owner])
+			.rpc({ skipPreflight, commitment: 'confirmed' });
+
+		const session = await program.account.sessionAccount.fetch(pda);
+		const events = await getTxEvents(provider, program, tx);
+
+		return { tx, pda, events, session };
+	};
 
 	before(async () => {
+		// initialize program global account
 		await program.methods
 			.initProgram()
 			.accounts({
-				owner: owner.publicKey,
+				owner: administrator.publicKey,
 				globalAccount: globalPDA,
 			})
 			// .signers([sessionAccount])
 			.rpc({ skipPreflight, commitment: 'confirmed' });
 
-		globalAccount = await program.account.globalAccount.fetch(globalPDA);
+		// request airdrop for voting actors
+		await connection.requestAirdrop(batman.publicKey, 1000000000);
+	});
+
+	describe('> createVotingSession', () => {
+		it('> should succeed when called with program administrator account', async () => {
+			const expectedSessionId = 0;
+			const name = 'Session A';
+			const description = 'A new session';
+
+			const {
+				session,
+				events: { sessionCreated, workflowStatusChanged },
+			} = await createVotingSession(administrator.payer, name, description);
+
+			assert.equal(session.sessionId.toNumber(), expectedSessionId);
+			assert.equal(session.name, name);
+			assert.equal(session.description, description);
+			assert.deepEqual(session.status, { registeringVoters: {} });
+
+			assert.equal(workflowStatusChanged.sessionId.toString(), expectedSessionId);
+			assert.deepEqual(workflowStatusChanged.previousStatus, { none: {} });
+			assert.deepEqual(workflowStatusChanged.currentStatus, {
+				registeringVoters: {},
+			});
+
+			assert.equal(sessionCreated.sessionId.toString(), expectedSessionId);
+			assert.equal(sessionCreated.name, name);
+			assert.equal(sessionCreated.description, description);
+		});
+
+		it('> should succeed when called with non administrator account', async () => {
+			const expectedSessionId = 1;
+			const name = 'Session A';
+			const description = 'A new session';
+
+			const {
+				session,
+				events: { sessionCreated, workflowStatusChanged },
+			} = await createVotingSession(batman, name, description);
+
+			assert.equal(session.sessionId.toNumber(), expectedSessionId);
+			assert.equal(session.name, name);
+			assert.equal(session.description, description);
+			assert.deepEqual(session.status, { registeringVoters: {} });
+
+			assert.equal(workflowStatusChanged.sessionId.toString(), expectedSessionId);
+			assert.deepEqual(workflowStatusChanged.previousStatus, { none: {} });
+			assert.deepEqual(workflowStatusChanged.currentStatus, {
+				registeringVoters: {},
+			});
+
+			assert.equal(sessionCreated.sessionId.toString(), expectedSessionId);
+			assert.equal(sessionCreated.name, name);
+			assert.equal(sessionCreated.description, description);
+		});
 	});
 
 	describe('> Voting actions are conditionned by voting session status', () => {
-		let sessionId = '0';
-		let createVotingSessionTx: string;
-
 		beforeEach(async () => {
-			const [sessionPDA] = PublicKey.findProgramAddressSync(
-				[Buffer.from('session'), globalAccount.sessionCount.toBuffer('le', 8)],
-				program.programId,
+			await createVotingSession(
+				administrator.payer,
+				'Super Heroes',
+				'A vote for every superheroes to find who will rule the world',
 			);
-
-			createVotingSessionTx = await program.methods
-				.createVotingSession(
-					'Super Heroes',
-					'A vote for every superheroes to find who will rule the world',
-				)
-				.accounts({
-					owner: owner.publicKey,
-					sessionAccount: sessionPDA,
-					globalAccount: globalPDA,
-				})
-				// .signers([sessionAccount])
-				.rpc({ skipPreflight, commitment: 'confirmed' });
-
-			globalAccount = await program.account.globalAccount.fetch(globalPDA);
 		});
 
-		context('## voting status is RegisteringVoters', () => {
-			describe('> createVotingSession', () => {
-				it('> should succeed when called with program administrator address', async () => {
-					const [sessionPDA] = PublicKey.findProgramAddressSync(
-						[Buffer.from('session'), globalAccount.sessionCount.toBuffer('le', 8)],
-						program.programId,
-					);
-
-					const expectedSessionId = '1';
-					const name = 'Session A';
-					const description = 'A new session';
-
-					createVotingSessionTx = await program.methods
-						.createVotingSession(name, description)
-						.accounts({
-							owner: owner.publicKey,
-							sessionAccount: sessionPDA,
-							globalAccount: globalPDA,
-						})
-						// .signers([sessionAccount])
-						.rpc({ skipPreflight, commitment: 'confirmed' });
-
-					const votingSession =
-						await program.account.sessionAccount.fetch(sessionPDA);
-
-					assert.equal(votingSession.sessionId.toString(), expectedSessionId);
-					assert.equal(votingSession.name, name);
-					assert.equal(votingSession.description, description);
-					assert.deepEqual(votingSession.status, { registeringVoters: {} });
-
-					const { workflowStatusChanged, sessionCreated } = await getTxEvents(
-						provider,
-						program,
-						createVotingSessionTx,
-					);
-
-					assert.equal(
-						workflowStatusChanged.sessionId.toString(),
-						expectedSessionId,
-					);
-					assert.deepEqual(workflowStatusChanged.previousStatus, { none: {} });
-					assert.deepEqual(workflowStatusChanged.currentStatus, {
-						registeringVoters: {},
-					});
-
-					assert.equal(sessionCreated.sessionId.toString(), expectedSessionId);
-					assert.equal(sessionCreated.name, name);
-					assert.equal(sessionCreated.description, description);
-				});
-			});
-		});
+		context('## voting status is RegisteringVoters', () => {});
 	});
 });
